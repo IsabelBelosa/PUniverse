9662a373d8c4d743db0e920ff8777027e1c3f8ef
IsabelBelosa <isabel.becerralosada@alum.uca.es>
2024-05-17 13:39:55 +0200

    cambiosDialogo

--- Assets/Scripts/TetrisScripts/GameManager.cs
+++ Assets/Scripts/TetrisScripts/GameManager.cs
@@ -4,55 +4,38 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class GameManager : MonoBehaviour
-{
-    public GameObject startButton, window, windowPause, pauseBtn;
+public class GameManager : MonoBehaviour {
+	public GameObject startButton, window, windowPause, pauseBtn;
 
-    DialogueTetrisScript dialogo;
+	// Use this for initialization
+	void Awake () {
+		Time.timeScale = 0; 
+	}
 
-    // Use this for initialization
-    void Start()
-    {
-        dialogo = GameObject.Find("DialogueTetrisScript").GetComponent<DialogueTetrisScript>();
-        startButton.GetComponent<Button>().interactable = false; // Deshabilitar el botón de inicio
-        dialogo.OnDialogueEnd.AddListener(EnableStartButton); // Suscribirse al evento para habilitar el botón de inicio al final del diálogo
-        dialogo.MostrarPanel(); // Mostrar el panel de diálogo
-    }
+	public void OnClickStart(){
+		Time.timeScale = 1; 
+		window.SetActive (false); 
+		this.gameObject.GetComponent<Movement> ().startGame (); 
+	}
 
-    void EnableStartButton()
-    {
-        startButton.GetComponent<Button>().interactable = true; // Habilitar el botón de inicio cuando el diálogo haya terminado
-    }
+	public void OnClickExit(){
+		Application.Quit (); 
+	}
 
-    public void OnClickStart()
-    {
-        Time.timeScale = 1;
-        window.SetActive(false);
-        this.gameObject.GetComponent<Movement>().startGame();
-    }
+	public void OnClickRestart(){
+		windowPause.SetActive (false); 
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name); 
+	}
 
-    public void OnClickExit()
-    {
-        Application.Quit();
-    }
+	public void OnClickPause(){
+		pauseBtn.GetComponent<Button> ().interactable = false; 
+		Time.timeScale = 0F;
+		windowPause.SetActive (true); 
+	}
 
-    public void OnClickRestart()
-    {
-        windowPause.SetActive(false);
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-    }
-
-    public void OnClickPause()
-    {
-        pauseBtn.GetComponent<Button>().interactable = false;
-        Time.timeScale = 0F;
-        windowPause.SetActive(true);
-    }
-
-    public void OnClickContinue()
-    {
-        pauseBtn.GetComponent<Button>().interactable = true;
-        Time.timeScale = 1F;
-        windowPause.SetActive(false);
-    }
+	public void OnClickContinue(){
+		pauseBtn.GetComponent<Button> ().interactable = true; 
+		Time.timeScale = 1F;
+		windowPause.SetActive (false); 
+	}
 }
